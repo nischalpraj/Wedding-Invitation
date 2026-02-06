cdb44f43b27d1a153fdfa880e71d596c88225ba2
nischalpraj <nischalpraj776@gmail.com>
2026-02-06 11:35:11 +0545

    Making scratch

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const canvas = document.getElementById("scratchCanvas");
 const ctx = canvas.getContext("2d");
+canvas.width = 100;
+canvas.height = 100;
+canvas.borderRadius = "100%";
+ctx.fillStyle = "gray";
